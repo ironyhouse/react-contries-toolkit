04d5d33f2980f7092d328cf1918eb9932ec45498
ironyhouse <ironylait@gmail.com>
2023-04-06 16:16:54 +0200

    update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,12 +4,12 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello World string', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/Hello World/i)).toBeInTheDocument();
 });
