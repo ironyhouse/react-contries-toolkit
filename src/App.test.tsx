387aba1c08a204326dc6210d55d7f419bbb9025f
ironyhouse <ironylait@gmail.com>
2023-04-15 17:22:01 +0200

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
-// test('renders Hello World string', () => {
-// const { getByText } = render(
-// <Provider store={store}>
-// <App />
-// </Provider>
-// );
-// expect(getByText(/Hello World/i)).toBeInTheDocument();
-// });
+test('renders Hello World string', () => {
+  render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  )
+
+  expect(screen.getByText(/Where is the world/i)).toBeInTheDocument()
+})
